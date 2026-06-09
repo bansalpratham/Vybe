bad62fd651b591e62a035721c04ae9b46b866d3a
bansalpratham <prathambansal200@gmail.com>
2026-06-10 00:22:57 +0530

    feat: Initialize backend server with Express and routing setup

--- Backend/index.js
+++ Backend/index.js
@@ -1,19 +1,19 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cookieParser from "cookie-parser"
-import connectDB from '../config/db.js'
+import connectDB from './config/db.js'
 import cors from 'cors'
-
-import authRouter from '../routes/auth.routes.js'
-import userRouter from '../routes/user.routes.js'
-import postRouter from '../routes/post.routes.js'
-import loopRouter from '../routes/loop.routes.js'
-import storyRouter from '../routes/story.routes.js'
-import notificationRouter from '../routes/notification.routes.js'
+import authRouter from './routes/auth.routes.js'
+import userRouter from './routes/user.routes.js'
+import postRouter from './routes/post.routes.js'
+import loopRouter from './routes/loop.routes.js'
+import storyRouter from './routes/story.routes.js'
+import notificationRouter from './routes/notification.routes.js'
 
 dotenv.config()
 
 const app = express()
+const port = process.env.PORT || 5000
 
 connectDB()
 
@@ -32,4 +32,6 @@ app.use("/api/loop", loopRouter)
 app.use("/api/story", storyRouter)
 app.use("/api/notification", notificationRouter)
 
-export default app
+app.listen(port, () => {
+    console.log(`server started`)
+})
