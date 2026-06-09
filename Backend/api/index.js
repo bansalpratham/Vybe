2dc011d2eb8bb49827fe589e3829b4c19ce6ef44
bansalpratham <prathambansal200@gmail.com>
2026-06-10 00:18:15 +0530

    feat: Set up API structure with routing and Vercel configuration

---
+++ Backend/api/index.js
@@ -0,0 +1,35 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import cookieParser from "cookie-parser"
+import connectDB from '../config/db.js'
+import cors from 'cors'
+
+import authRouter from '../routes/auth.routes.js'
+import userRouter from '../routes/user.routes.js'
+import postRouter from '../routes/post.routes.js'
+import loopRouter from '../routes/loop.routes.js'
+import storyRouter from '../routes/story.routes.js'
+import notificationRouter from '../routes/notification.routes.js'
+
+dotenv.config()
+
+const app = express()
+
+connectDB()
+
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true
+}))
+
+app.use(express.json())
+app.use(cookieParser())
+
+app.use("/api/auth", authRouter)
+app.use("/api/user", userRouter)
+app.use("/api/post", postRouter)
+app.use("/api/loop", loopRouter)
+app.use("/api/story", storyRouter)
+app.use("/api/notification", notificationRouter)
+
+export default app
